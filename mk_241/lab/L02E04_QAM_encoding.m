440f40ce923f7cd5f7bd3c20dcbe5aad0afec6e8
akapet00 <akapet00@gmail.com>
2023-03-29 11:11:29 +0200

    updates

--- mk_241/lab/L02E04_QAM_encoding.m
+++ mk_241/lab/L02E04_QAM_encoding.m
@@ -1,5 +1,67 @@
-k = 4;
+%% modulate data using QAM and display results in a scatter plot
+k = 4;                          % number of bits
+M = 2^k;                        % order of QAM -> 16QAM
+x = (0:M-1)';                   % input data
+y = qammod(x, M);
+scatterplot(y)
+title('16-QAM')
+
+%% normalize QAM signal by average power
+k = 5;
 M = 2^k;
-y = int2bit((0:M-1)',k,false);
-symorder = 'gray';
-xmap = qammod(y,M,symorder,'InputType','bit','PlotConstellation',true);
+x = randi([0 M-1], 1000, 1);    % 1000 random integers
+y = qammod(x, M, 'UnitAveragePower', true);
+avgPower = mean(abs(y).^2);
+scatterplot(y)
+title('64-QAM, Average Power = 1 W')
+
+%% binary ordering
+M = 16;
+d = (0:M-1);
+symorder = 'bin';
+qammod(d, M, 'PlotConstellation', true);
+qammod(d, M, symorder, 'PlotConstellation', true);
+
+%% bit as inputs
+k = 6;
+M = 2^k;
+d = (0:M-1);
+y = transpose(de2bi(d, k));
+symorder = 'bin';
+xmap = qammod(y, M, symorder, ...
+    'InputType', 'bit', ...
+    'PlotConstellation', true);
+
+%% noisy channel 
+k = 5;
+M = 2^k;
+% generate binary data sequence
+% the number of rows, N, in the input must be an integer multiple of
+% the number of bits per symbol, k
+x = randi([0 1], 1000*k, 1);
+% modulate the signal using bit inputs, set it to have unit average power
+tx = qammod(x, M, 'InputType', 'bit', ...
+    'UnitAveragePower', true);
+% pass the signal through a noisy channel
+rx = awgn(tx, 25);
+% plot the constellation diagram
+cd = comm.ConstellationDiagram( ...
+    'ShowReferenceConstellation', false);
+cd(rx)
+
+%% demodulation
+% set the modulation order as 64, and determine the number of bits/symbol
+M = 64;
+k = log2(M);
+% generate a sequence of random bits considering the number of bits/symbol
+x = randi([0 1], 10*k, 1);
+% modulate the input data using a binary symbol mapping and
+% set the modulator to output fixed-point data;
+% the numeric data type is signed with a 16-bit word length and
+% a 10-bit fraction length
+y = qammod(x, M, 'bin', ...
+    'InputType', 'bit', ...
+    'OutputDataType', numerictype(1,16,10));
+% demodulate the modulated signal
+z = qamdemod(y, M, 'bin', 'OutputType', 'bit');
+s = isequal(x, boolean(z))
--- mk_241/lab/L03E02_symbol_error_rate.m
+++ mk_241/lab/L03E02_symbol_error_rate.m
@@ -5,28 +5,29 @@
 
 % Set alphabet size for 16-QAM
 M = 16;
-x = randi([0 M-1],5000,1);
+x = randi([0 M-1], 5000, 1);
+% initialize constellation diagram
+c = qammod(0:M-1, M);
+cd = comm.ConstellationDiagram('ReferenceConstellation', c, ...
+    'XLimits', [-4 4], ...
+    'YLimits', [-4 4]);
+% apply 16-QAM modulation
+tx= qammod(x, M);
 
-% Initialize constellation diagram
-cpts = qammod(0:M-1,M);
-constDiag = comm.ConstellationDiagram('ReferenceConstellation',cpts, ...
-    'XLimits',[-4 4],'YLimits',[-4 4]);
+% pass the signal through an AWGN channel with 
+% the SNR set to 15 and
+% the parameter SIGPOWER defined as 'measured'
+rx = awgn(tx, 15, 'measured');
 
-% Apply 16-QAM modulation
-y = qammod(x,M);
-
-% Transmit signal through an AWGN channel with SNR set to 15 and SIGPOWER
-% defined as 'measured'
-ynoisy = awgn(y,15,'measured');
-
-% Demodulate ynoisy to recover the message
-z = qamdemod(ynoisy,M);
+% demodulate the modeulated signal to recover the message
+z = qamdemod(rx, M);
 
 % Check the symbol error rate
-[num,rt] = symerr(x,z);
+[num, rt] = symerr(x, z);
 
-% Create constellation diagram from noisy data. The signal reference
-% constellation has 16 precisely located points but the transmitted symbols
-% with the noise added causes the scatter plot to have a small cluster of
-% points scattered around each reference constellation point.
-constDiag(ynoisy)
+% create constellation diagram from noisy data;
+% note that the signal reference constellation has 16 precisely located
+% points, but the transmitted symbols with the noise added causes the
+% scatter plot to have a small cluster of points scattered around each
+% reference constellation point
+cd(rx)
